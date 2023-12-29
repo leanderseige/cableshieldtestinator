1c6a9013f13a2057180e046fedab16a79bfe75f7
Leander Seige <leander@seige.name>
2023-12-29 09:21:46 +0100

    Update code.ino.ino

--- src/code.ino/code.ino.ino
+++ src/code.ino/code.ino.ino
@@ -2,10 +2,22 @@
 
 Cableshieldtestinator
 
-(c) 2023, Leander Seige
+This test device attempts to determine non-invasively whether a cable is shielded or not.
 
-This is the software for a 
+Copyright (C) 2023 Leander Seige
 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 // #define DEBUG 1
